88d0aaed4d7a173340077ac7ac15b9dbf3ab45d7
Gustavo Moura <gustavo_monteiro.20@hotmail.com>
2023-08-22 09:55:14 -0300

    Criando BDD

---
+++ PiasENichos/Form1.cs
@@ -0,0 +1,10 @@
+namespace PiasENichos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Querys/CREATE_TABLE.sql
@@ -0,0 +1,16 @@
+use db_PiasNichos
+go
+
+--CREATE TABLE tbl_Categorias (
+--	id_Categoria INT PRIMARY KEY IDENTITY,
+--	ds_Categoria VARCHAR(50) NOT NULL
+--)
+
+--CREATE TABLE tbl_Produtos (
+--	id_Produto INT PRIMARY KEY IDENTITY,
+--	ds_Produto VARCHAR(80),
+--	co_Cor VARCHAR(50),
+--	vl_Valor DECIMAL(15,2),
+--	id_Categoria INT
+--	FOREIGN KEY (id_Categoria) REFERENCES tbl_Categorias (id_Categoria)
+--)
